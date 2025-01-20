710e08779d0d49f4950114dbc6a6a026d013124a
Andrew <acw0151@auburn.edu>
2025-01-20 08:37:45 -0600

    Assignment 2 Complete 1.20.25

--- Module2/AndrewWardAssignment2.playground/Contents.swift
+++ Module2/AndrewWardAssignment2.playground/Contents.swift
@@ -1,5 +1,6 @@
 /*
  Simple playground for a mortgage payment calculator.
+ Calculates the payment for the two provided examples
  CSPC-5330 Assignment 2
  Author: Andrew Ward
  Version: 1.19.25
@@ -14,11 +15,22 @@ func Payment(principal: Int, payments: Int, apr: Float) -> Float {
     return monthlyPayment
 }
 
-print("Below are two examples of mortgage payment calculation\n")
+print("Below are two examples of mortgage payment calculations\n")
 print("Example 1:\n")
 print("Principal - $20,000\n")
 print("Period - 2 Months\n")
-print("Intrest Rate - 4.4% APR Compounded Monthly\n")
+print("Interest Rate - 4.4% APR Compounded Monthly\n")
 
-print("Monthly Payment: ",Payment(principal: 200000, payments: 360, apr: 0.005416667))
+print("Monthly Payment: ","$",Payment(principal: 20000, payments: 2, apr: 0.00367),"\n")
+
+print("--------------------------------------------------------------------------------\n")
+
+print("Example 2:\n")
+print("Principal - $150,000\n")
+print("Period - 30 Years\n")
+print("Interest Rate - 5.0% APR with one annual payment\n")
+
+print("Yearly Payment: ","$",Payment(principal: 150000, payments: 30, apr: 0.05),"\n")
+
+
 
