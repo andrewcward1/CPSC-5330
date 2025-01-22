5a0ba08f7f09c10c0e46df11c446f549339f9302
Andrew <acw0151@auburn.edu>
2025-01-22 17:59:44 -0600

    Assignment 3 Initial Commit 1.22.25

---
+++ Module1/Assignment1/Assignment1Tests/Assignment1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1Tests.swift
+//  Assignment1Tests
+//
+//  Created by Andrew Ward on 1/15/25.
+//
+
+import Testing
+@testable import Assignment1
+
+struct Assignment1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
